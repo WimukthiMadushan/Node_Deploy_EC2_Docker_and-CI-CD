6d8e4bd47a1eeb3fab5ad4ed3ecec6324b60cf06
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-02-02 19:50:07 +0530

    complete

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,7 @@
-# Use an existing image as a base
-FROM node:14
-
-# Set the working directory
-WORKDIR /usr/src/app
-
-# Copy the package.json and package-lock.json files
-COPY package*.json ./
-
-# Install the dependencies
+FROM node:16.20.1
+WORKDIR /app 
+COPY package.json ./
 RUN npm install
-
-# Copy the rest of the code
 COPY . .
-
-# Expose the port that the app listens on
 EXPOSE 3000
-
-# Define the command to run the app
-CMD ["node", "server.js"]
+CMD ["npm","run", "start"]
--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
-//
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connacting database', err);
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+}, { collection: 'user' })
+
+const User = mongoose.model('User', userSchema);
+
+
+app.get('/api/user', async (req, res) => {
+    try {
+        const users = await User.find();  
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching users", error: err });
+    }
+});
+
+
 
 // Start the server
 app.listen(PORT, () => {
